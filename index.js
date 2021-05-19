bb151b7a26dd9c18027bed4ea0921fb7e9e1307f
garrettBourque <bourque.garrett@gmail.com>
2021-05-19 15:29:55 -0500

    completed assignment

--- index.js
+++ index.js
@@ -11,6 +11,44 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+//books.map(elem => elem.price);
+
+
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(titling);
+}
+
+//const titleCased = tutorials.map(elem=>titling(elem))
+
+
+
+function titling(title){
+  let retitled = '';
+  retitled+=replaceAt(0,title.charAt(0),title);
+
+  let index=1;
+  
+  let test =index<title.length;
+
+  //look at using indexOf() to search for next space. then change
+  
+
+  while(test){
+    
+   
+    if(retitled.charAt(index)===(' ')){
+      
+      retitled = replaceAt(index+1,retitled.charAt(index+1),retitled)
+    }
+    
+    index++;
+    test =index<title.length;
+  }
+  
+  return retitled;
+}
+
+function replaceAt(index,letter,title){
+
+  return title.substr(0,index)+letter.toUpperCase()+title.substr(index+1);
 }
